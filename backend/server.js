4c27faa80bdcec350f2b70b68294867c0dbde02e
chian <hsiao020729528@gmail.com>
2023-08-01 16:54:07 +0800

    backend init

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import linebot from 'linebot';
+import dotenv from 'dotenv-defaults';
+
+dotenv.config();
+
+var bot = linebot({
+  channelId: process.env.LINE_CHANNEL_ID,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
+});
+
+bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    console.log("Get a message and Success!");
+  }).catch(function (error) {
+    console.log("Get a message and catch an error");
+    console.log(error);
+  });
+});
+
+bot.listen('/linewebhook', 80);
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/linewebhook', linebotParser);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, ()=> console.log(`Listening on http://localhost:${port}`));
